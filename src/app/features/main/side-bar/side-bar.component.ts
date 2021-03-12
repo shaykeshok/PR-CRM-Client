54f98d68ff223f2272cc22a93e8d2d4de5cdf9ee
shaykeshok <shok.shayke@gmail.com>
2021-03-12 14:16:37 +0200

    finish design sidebar

--- src/app/features/main/side-bar/side-bar.component.ts
+++ src/app/features/main/side-bar/side-bar.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AppService } from 'src/app/service/app.service';
+// export interface INav {
+//   label: string;
+//   // action: TSearchOption;
+//   icon?: string;
+//   // count?: Observable<number>;
+// }
+export interface INav {
+  label: string;
+  action?: string;
+  icon?: string;
+}
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +18,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
+  @Input() isMobile = false;
+  iconPath = 'assets/icons/';
+  public get version(): string {
+    return this.app.version;
+  }
+  public get navs(): INav[] {
+    return this._navs;
+  }
+  private _navs: INav[] = [
+    {
+      label: 'Dashboard',
+      action: 'dashboard',
+      icon: 'dashboard.png'
+    }, {
+      label: 'Templates',
+      action: 'templates',
+      icon: 'templates.png'
+    }, {
+      label: 'Reports',
+      action: 'reports',
+      icon: 'reports.png'
+    }, {
+      label: 'Data',
+      action: 'data',
+      icon: 'database.svg'
+    }
+  ];
 
-  constructor() { }
-
+  btn = {
+    setting: 'settings',
+    signout: 'signout',
+  };
+  constructor(private app: AppService) { }
   ngOnInit() {
   }
 
+
+  onActionClick(action){
+
+  }
+  signout(){}
 }
